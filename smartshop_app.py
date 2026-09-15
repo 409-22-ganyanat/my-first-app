260702f1f36fb53d93a2a400b68d1a26bb2214ee
409-22-ganyanat <54434@yupparaj.ac.th>
2026-09-15 14:02:42 +0700

    Update smartshop_app.py

--- smartshop_app.py
+++ smartshop_app.py
@@ -1,1 +1,13 @@
-
+import streamlit as st
+st.title("Khai Kue Chiwit (Eggs are life)")
+st.divider()
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+vat = price*0.07
+net_price = price-vat
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider()
+st.write("นางสาวกัญญาวีร์ แสนคำ เลขที่ 19 ม.4/9")
+st.write("นางสาวกัญญานัท ละอำ เลขที่ 22 ม.4/9")
+st.write("นางสาวณัฏฐณิชชา สัจจ์ธัมม์ เลขที่ 23 ม.4/9")
+st.write("นางสาวปิโยรส แย้มเอิบสิน เลขที่ 45 ม.4/9")
